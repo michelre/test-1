6ffc6e92fd302e336ac9bcb6b3ccce1450a35a67
Rémi Michel <remi.michel38@gmail.com>
2022-10-17 19:01:50 +0200

    feat(): Session du 17/10

---
+++ README.md
@@ -0,0 +1,13 @@
+# KANAP
+
+Pour démarrer le serveur:
+
+```
+cd back
+npm start
+```
+
+Les routes de l'API:
+- http://localhost:3000/api/products permet de récupérer tous les produits
+- http://localhost:3000/api/products/:id permet de récupérer les informations d'un seul produit
+- http://localhost:3000/api/order permet d'envoyer une commande
--- js/product.js
+++ js/product.js
@@ -1,6 +1,43 @@
 /**
- * 1. Récupérer les infos du produit en question (fetch)
- * 2. Afficher les informations (nom, description, image, prix) du produit
- * 3. Afficher les variantes de produit
+ * 1. Récupérer les infos du produit en question (fetch) OK
+ * 2. Afficher les informations (nom, description, image, prix) du produit OK
+ * 3. Afficher les variantes de produit OK
  * 4. Ajouter un évènement d'ajout au panier (à revoir car utilisation du LocalStorage)
  */
+
+// On parse l'URL pour en déduire le paramètre id
+const url = new URL(window.location.href);
+
+// On appel l'API pour récupérer les informations du produit
+fetch('http://localhost:3000/api/products/' + url.searchParams.get('id'))
+    .then(function(res){
+        return res.json()
+    })
+    .then(function(product){
+        // On appelle la fonction displayProduct avec le produit récupéré depuis l'API
+        displayProduct(product)
+    })
+
+function displayProduct(product){
+    // On récupère les éléments du DOM
+    const title = document.querySelector('#title')
+    const price = document.querySelector('#price')
+    const description = document.querySelector('#description')
+    const image = document.querySelector('.item__img')
+    const colors = document.querySelector('#colors')
+
+    // On complète les informations titre, prix, description et image à partir des informations du produit
+    title.textContent = product.name
+    price.textContent = product.price
+    description.textContent = product.description
+    //image.innerHTML = '<img src="' + product.imageUrl + '" alt="' + product.altTxt + '"/>'
+    image.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}"/>`
+
+    // On complète les options du select avec les couleurs du produit
+    for(let i = 0; i < product.colors.length; i++){
+        const option = document.createElement('option')
+        option.textContent = product.colors[i]
+        option.setAttribute('value', product.colors[i])
+        colors.appendChild(option)
+    }
+}
