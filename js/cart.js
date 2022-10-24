4047d1fd39dfaef29a64e64652972341af875294
Rémi Michel <remi.michel38@gmail.com>
2022-10-24 19:36:12 +0200

    feat(): Session du 24/10

---
+++ js/cart.js
@@ -0,0 +1,9 @@
+/**
+ * 1. A partir du panier en localStorage, créer visuellement le panier (Nom, photo, quantité, prix total)
+ * 2. Supprimer des produits du panier (recalcul du total)
+ * 3. Modifier les quantités des produits du panier (recalcul du total)
+ * 4. Supprimer tous les éléments du panier
+ * 5. Valider le formulaire de commande
+ * 6. Envoyer le formulaire de commande (création d'un identifiant de commande)
+ * 7. Rediriger vers la page de confirmation de commande
+ */
