b743fc3a4586e1e409b1ba7e226e1ee4bea4c774
Rémi Michel <remi.michel38@gmail.com>
2022-11-07 19:36:30 +0100

    feat(): Session du 7/11

--- js/cart.js
+++ js/cart.js
@@ -2,28 +2,21 @@
  * 1. A partir du panier en localStorage, créer visuellement le panier (Nom, photo, quantité, prix total)
  * 2. Supprimer des produits du panier (recalcul du total)
  * 3. Modifier les quantités des produits du panier (recalcul du total)
- * 4. Supprimer tous les éléments du panier
- * 5. Valider le formulaire de commande
- * 6. Envoyer le formulaire de commande (création d'un identifiant de commande)
- * 7. Rediriger vers la page de confirmation de commande
+ * 4. Valider le formulaire de commande
+ * 5. Envoyer le formulaire de commande (création d'un identifiant de commande)
+ * 6. Rediriger vers la page de confirmation de commande
  */
 
 
 // On récupère le panier du localStorage
-const cart = JSON.parse(localStorage.getItem('cart') || '[]')
+let cart = JSON.parse(localStorage.getItem('cart') || '[]')
 //On récupère la section du DOM dans laquelle on va inclure les articles du panier
 const cartItems = document.querySelector('#cart__items')
 //On fait une boucle sur tous les articles du panier
 for(let i = 0; i < cart.length; i++){
     // Pour chaque article, on l'ajoute à la section cartItems
     const article = cart[i]
-    cartItems.innerHTML += createProductItem(article)
-    // TODO: Ajout des évènements
-    /*const articleDOM = document.querySelector(`[data-id="${article._id}"][data-color="${article.color}"]`)
-    const deleteButton = articleDOM.querySelector('.deleteItem')
-    deleteButton.addEventListener('click', function(){
-        console.log('click')
-    })*/
+    cartItems.appendChild(createProductItem(article))
 }
 
 
@@ -59,27 +52,77 @@ function calculateTotal(){
  * @returns {string}
  */
 function createProductItem(article){
-    return `<article class="cart__item" data-id="${article._id}" data-color="${article.color}">
-                <div class="cart__item__img">
-                  <img src="${article.imageUrl}" alt="Photographie d'un canapé">
-                </div>
-                <div class="cart__item__content">
-                  <div class="cart__item__content__description">
-                    <h2>${article.name}</h2>
-                    <p>${article.color}</p>
-                    <p>${article.price} €</p>
-                  </div>
-                  <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                      <p>Qté : </p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${article.quantity}">
-                    </div>
-                    <div class="cart__item__content__settings__delete">
-                      <p class="deleteItem">Supprimer</p>
-                    </div>
-                  </div>
-                </div>
-              </article>`
+    const articleElement = document.createElement('article')
+    articleElement.classList.add('cart__item')
+    articleElement.setAttribute('data-id', article._id)
+    articleElement.setAttribute('data-color', article.color)
+
+    const cartItemImg = document.createElement('div')
+    cartItemImg.classList.add('cart__item__img')
+    cartItemImg.innerHTML = `<img src="${article.imageUrl}" alt="Photographie d'un canapé">`
+
+    const cartItemContent = document.createElement('div')
+    cartItemContent.classList.add('cart__item__content')
+    const cartItemContentDescription = document.createElement('div')
+    cartItemContentDescription.classList.add('cart__item__content__description')
+    cartItemContentDescription.innerHTML = `
+       <h2>${article.name}</h2>
+       <p>${article.color}</p>
+       <p>${article.price} €</p>`
+    cartItemContent.appendChild(cartItemContentDescription)
+
+    const cartItemContentSettings = document.createElement('div')
+    cartItemContentSettings.classList.add('cart__item__content__settings')
+    const cartItemContentSettingsInput = document.createElement('input')
+    const cartItemContentSettingsP = document.createElement('p')
+    cartItemContentSettingsP.innerText = 'Qté :'
+    cartItemContentSettingsInput.setAttribute('type', 'number')
+    cartItemContentSettingsInput.setAttribute('name', 'itemQuantity')
+    cartItemContentSettingsInput.setAttribute('min', '1')
+    cartItemContentSettingsInput.setAttribute('max', '100')
+    cartItemContentSettingsInput.value = article.quantity
+    cartItemContentSettingsInput.classList.add('itemQuantity')
+
+    const deleteItem = document.createElement('p')
+    deleteItem.classList.add('deleteItem')
+    deleteItem.innerText = 'Supprimer'
+
+    const cartItemContentSettingsQuantity = document.createElement('div')
+    cartItemContentSettingsQuantity.classList.add('cart__item__content__settings__quantity')
+    const cartItemContentSettingsDelete = document.createElement('div')
+    cartItemContentSettingsDelete.classList.add('cart__item__content__settings__delete')
+
+    cartItemContentSettingsQuantity.appendChild(cartItemContentSettingsP)
+    cartItemContentSettingsQuantity.appendChild(cartItemContentSettingsInput)
+    cartItemContentSettingsDelete.appendChild(deleteItem)
+
+    cartItemContent.appendChild(cartItemContentSettingsQuantity)
+    cartItemContent.appendChild(cartItemContentSettingsDelete)
+
+    articleElement.appendChild(cartItemImg)
+    articleElement.appendChild(cartItemContent)
+
+
+    deleteItem.addEventListener('click', () => {
+        // Supprimer l'élément du localStorage
+        cart = cart.filter((articleCart) => !(articleCart._id === article._id && articleCart.color === article.color))
+        //Supprimer l'élément du DOM
+        articleElement.remove()
+        // ON recalcule le total en appelant la fonction
+        calculateTotal()
+        // On sauvegarde le panier dans le localStorage
+        localStorage.setItem('cart', JSON.stringify(cart))
+    })
+
+    cartItemContentSettingsInput.addEventListener('change', (e) => {
+        const articleCart = cart.find((a) => a._id === article._id && a.color === article.color)
+        articleCart.quantity = parseInt(e.target.value)
+        calculateTotal()
+        // On sauvegarde le panier dans le localStorage
+        localStorage.setItem('cart', JSON.stringify(cart))
+    })
+
+    return articleElement;
 }
 
 calculateTotal()
