d57bbeabadea2ff1119d1871e47b18a47515c1ea
mary elodie <99305063+MARYKKKK@users.noreply.github.com>
2022-11-09 02:35:54 +0100

    ajout pannier

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,5 @@
 // On récupère le panier du localStorage
-const cart = JSON.parse(localStorage.getItem('cart') || '[]')
+let cart = JSON.parse(localStorage.getItem('cart') || '[]')
 //On récupère la section du DOM dans laquelle on va inclure les articles du panier
 const cartItems = document.querySelector('#cart__items')
 //On fait une boucle sur tous les articles du panier
@@ -13,23 +13,24 @@ for(let i = 0; i < cart.length; i++){
     deleteButton.addEventListener('click', function(){
         console.log('click')
     })*/
+    cartItems.appendChild(createProductItem(article))
 }
 
 
 function calculateTotal(){
-    /*const cartTotals = cart
+    const cartTotals = cart
         .map(function(article){
             return article.price * article.quantity
         })
         .reduce(function(total, current){
             return total + current
-        }, 0)*/
+        }, 0)
 
-    /*const cartTotals = cart
+    const cartTotals = cart
         .reduce(function(total, current){
             return total + (current.price * current.quantity)
         }, 0)
-    */
+    
 
     let total = 0;
     for(let i = 0; i < cart.length; i++){
@@ -69,6 +70,78 @@ function createProductItem(article){
                   </div>
                 </div>
               </article>`
+
+    const articleElement = document.createElement('article')
+    articleElement.classList.add('cart__item')
+    articleElement.setAttribute('data-id', article._id)
+    articleElement.setAttribute('data-color', article.color)
+
+    const cartItemImg = document.createElement('div')
+    cartItemImg.classList.add('cart__item__img')
+    cartItemImg.innerHTML = `<img src="${article.imageUrl}" alt="Photographie d'un canapé">`
+
+    const cartItemContent = document.createElement('div')
+    cartItemContent.classList.add('cart__item__content')
+    const cartItemContentDescription = document.createElement('div')
+    cartItemContentDescription.classList.add('cart__item__content__description')
+    cartItemContentDescription.innerHTML = `
+       <h2>${article.name}</h2>
+       <p>${article.color}</p>
+       <p>${article.price} €</p>`
+    cartItemContent.appendChild(cartItemContentDescription)
+
+    const cartItemContentSettings = document.createElement('div')
+    cartItemContentSettings.classList.add('cart__item__content__settings')
+    const cartItemContentSettingsInput = document.createElement('input')
+    const cartItemContentSettingsP = document.createElement('p')
+    cartItemContentSettingsP.innerText = 'Qté :'
+    cartItemContentSettingsInput.setAttribute('type', 'number')
+    cartItemContentSettingsInput.setAttribute('name', 'itemQuantity')
+    cartItemContentSettingsInput.setAttribute('min', '1')
+    cartItemContentSettingsInput.setAttribute('max', '100')
+    cartItemContentSettingsInput.value = article.quantity
+    cartItemContentSettingsInput.classList.add('itemQuantity')
+
+    const deleteItem = document.createElement('p')
+    deleteItem.classList.add('deleteItem')
+    deleteItem.innerText = 'Supprimer'
+
+    const cartItemContentSettingsQuantity = document.createElement('div')
+    cartItemContentSettingsQuantity.classList.add('cart__item__content__settings__quantity')
+    const cartItemContentSettingsDelete = document.createElement('div')
+    cartItemContentSettingsDelete.classList.add('cart__item__content__settings__delete')
+
+    cartItemContentSettingsQuantity.appendChild(cartItemContentSettingsP)
+    cartItemContentSettingsQuantity.appendChild(cartItemContentSettingsInput)
+    cartItemContentSettingsDelete.appendChild(deleteItem)
+
+    cartItemContent.appendChild(cartItemContentSettingsQuantity)
+    cartItemContent.appendChild(cartItemContentSettingsDelete)
+
+    articleElement.appendChild(cartItemImg)
+    articleElement.appendChild(cartItemContent)
+
+
+    deleteItem.addEventListener('click', () => {
+        // Supprimer l'élément du localStorage
+        cart = cart.filter((articleCart) => !(articleCart._id === article._id && articleCart.color === article.color))
+        //Supprimer l'élément du DOM
+        articleElement.remove()
+        // ON recalcule le total en appelant la fonction
+        calculateTotal()
+        // On sauvegarde le panier dans le localStorage
+        localStorage.setItem('cart', JSON.stringify(cart))
+    })
+
+    cartItemContentSettingsInput.addEventListener('change', (e) => {
+        const articleCart = cart.find((a) => a._id === article._id && a.color === article.color)
+        articleCart.quantity = parseInt(e.target.value)
+        calculateTotal()
+        // On sauvegarde le panier dans le localStorage
+        localStorage.setItem('cart', JSON.stringify(cart))
+    })
+
+    return articleElement;
 }
 
 calculateTotal()
